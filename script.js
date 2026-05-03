8860fd8d8261473ccde86458bbd74c57d5c2ce77
TomHacker69 <ankitomar.shs@gmail.com>
2026-05-03 15:07:13 +0530

    Implement renderMarkdown function for Markdown parsing
    Add a function to render Markdown content with HTML tags

--- script.js
+++ script.js
@@ -594,3 +594,14 @@ function renderContentWithLinks(content) {
     return `<span class="wiki-link" onclick="selectNote('${note.id}')">${title}</span>`;
   });
 }
+function renderMarkdown(content) {
+  return renderContentWithLinks(content)
+    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
+    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
+    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
+    .replace(/\*\*(.*?)\*\*/gim, "<strong>$1</strong>")
+    .replace(/\*(.*?)\*/gim, "<em>$1</em>")
+    .replace(/`(.*?)`/gim, "<code>$1</code>")
+    .replace(/^- (.*$)/gim, "<li>$1</li>")
+    .replace(/\n/g, "<br>");
+}
